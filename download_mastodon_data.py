9da238fe8cbf2018a363a149f040d8a2d562b47d
Poltergeist <77846583+nmeisters@users.noreply.github.com>
2023-04-08 15:51:40 +0200

    Initialize

---
+++ download_mastodon_data.py
@@ -0,0 +1,86 @@
+import pathlib
+import time
+import threading
+import orjson as json
+
+import mastodon
+from mastodon.streaming import StreamListener, CallbackStreamListener
+from ftlangdetect import detect
+
+
+def activate_account(login, email, password):
+    nick, domain = login.split("@")
+    server_url = f'https://{domain}'
+    app_name = f'{nick}_app'
+    client_file = f'{nick}_clientcred.secret'
+    user_file = f'{nick}_usercred.secret'
+    if not pathlib.Path(client_file).exists():
+        mastodon.Mastodon.create_app(
+            app_name,
+            api_base_url=server_url,
+            to_file=client_file)
+        print(f'>>- Created App for {nick}')
+    api = mastodon.Mastodon(client_id=client_file)
+    if not pathlib.Path(user_file).exists():
+        api.log_in(
+            email,
+            password,
+            to_file=user_file)
+        print(f'>>- Created Token for {nick}')
+    api = mastodon.Mastodon(access_token=user_file)
+    return api
+
+def save_toot(toot, filename):
+    with open(f"fediverse-{filename}-toots.log", "ba") as tootfile:
+        tootfile.write(json.dumps(toot) + b"\n")
+
+
+def print_toot(toot):
+    print(toot)
+    # if toot.get('language') == 'de':
+    # lang = detect(text=toot['content']).get('lang')
+    # if lang == 'de':
+    #     print(toot['content'])
+
+
+def handle_toot(toot, filename):
+    save_toot(toot, filename)
+    # print_toot(toot)
+
+
+def run_listener(login, email, password):
+    api = activate_account(login, email, password)
+    filename = login.split("@")[-1]
+    listener = CallbackStreamListener(
+        update_handler=lambda x: handle_toot(x, filename),
+        local_update_handler=lambda x: handle_toot(x, filename),
+        status_update_handler=lambda x: handle_toot(x, filename),
+    )
+    print(f'>>- Streaming {filename}')
+    while True:
+        try:
+            api.stream_public(listener=listener)
+        except mastodon.errors.MastodonNetworkError as exc:
+            if 'Not Found' in str(exc):
+                print(f'>>- Cannot connect to {filename}, aborting')
+                return
+            print(f'>>- Reconnecting {filename} ...')
+
+
+
+
+if __name__ == "__main__":
+    threads = []
+    for account in accounts:
+        login, email, password = account
+        t = threading.Thread(target=run_listener,
+                             args=(login, email, password,))
+        threads.append(t)
+        t.start()
+    while True:
+        try:
+            time.sleep(5)
+        except Exception as exc:
+            print(f'Quitting: {exc}')
+        for t in threads:
+            t.join()
